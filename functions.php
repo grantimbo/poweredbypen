0605e106fb45cb7af0226c3c84faa68d62ca9761
Grant Imbo <grant.imbo@gmail.com>
2019-05-23 10:16:14 +0800

    bug - just removed the fade animation to fix

--- functions.php
+++ functions.php
@@ -116,7 +116,7 @@ function header_styles() {
     wp_register_style('normalize', get_template_directory_uri() . '/css/normalize.min.css', '2.1.3', 'all');
     wp_enqueue_style('normalize'); // Enqueue it!
 
-    wp_register_style('grantimbo', get_template_directory_uri() . '/style.css', array(), '1.3', 'all');
+    wp_register_style('grantimbo', get_template_directory_uri() . '/style.css', array(), '1.1.5', 'all');
     wp_enqueue_style('grantimbo'); // Enqueue it!
 
     // wp_dequeue_style( 'wp-block-library' ); // Remove Gutenberg css
