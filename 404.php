19070f6eb9484266aab8f5215141938351bc572b
Grant Imbo <grant.imbo@gmail.com>
2019-05-24 04:30:51 +0800

    optimized assets

--- 404.php
+++ 404.php
@@ -1,18 +1,4 @@
-<!doctype html>
-<html <?php language_attributes(); ?> >
-	<head>
-		<meta charset="<?php bloginfo('charset'); ?>">
-		<title><?php wp_title(''); ?><?php if(wp_title('', false)) { echo ' &mdash; '; } ?><?php bloginfo('description'); ?></title>
-
-		<link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
-		<link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
-
-		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-		<meta name="viewport" content="width=device-width,initial-scale=1.0">
-		
-        <?php wp_head(); ?>
-
-</head>
+<?php get_header(); ?>
 
 <body <?php body_class(); ?>>
 
@@ -24,4 +10,7 @@
 		<p>The page you are looking for does not exist. <br>It may have been moved, or removed altogether. <br>You may return to the <a color="#ccc" href="<?php echo home_url(); ?>"><?php _e( 'home page.', 'html5blank' ); ?></a></p>
 	</main>
 
-<?php wp_footer(); ?>
+	<?php wp_footer(); ?>
+</body>
+</html>
+
--- index.php
+++ index.php
@@ -1,28 +1,75 @@
 <?php get_header(); ?>
-<main class="wrap" role="main">
-	<section class="home-wrap home">
-        <div class="home-content">
-            <div class="slide-controls">
-                <a class="prev-slide"><i class="icon-navigate_before"></i></a>
-                <a class="next-slide"><i class="icon-navigate_next"></i></a>
+
+<body data-post="<?php echo the_ID(); ?>" <?php body_class(); ?>>
+
+    <aside class="sidebar">
+        <a data-nav="home" class="logo"  href="<?php echo site_url(); ?>">
+            <img src="<?php bloginfo('template_url'); ?>/img/logo.png" alt="Powered by Pen">
+        </a>
+        <p class="tagline">Ash <span>X</span> Grant</p>
+
+        <div class="menu-button"><i class="icon-dehaze"></i></div>
+        <div class="mobile-menu">
+            <div class="menu-button-close"><i class="icon-close"></i></div>
+            <nav class="nav">
+                <ul>
+                    <li><a data-nav="portfolio" href="/portfolio">Portfolio</a></li>
+                    <li><a data-nav="about" href="/about">About</a></li>
+                    <li><a data-nav="contact" href="/contact">Contact</a></li>
+                </ul>
+            </nav>
+            <nav class="social-links clear">
+                <a class="social-facebook" href="//facebook.com/poweredbypen" target="_blank"><i class="icon-facebook"></i></a>
+                <a class="social-artstation" href="//artstation.com/poweredbypen" target="_blank"><i class="icon-artstation"></i></a>
+                <a class="social-instagram" href="//instagram.com/poweredbypen" target="_blank"><i class="icon-instagram"></i></a>
+                <a class="social-youtube" href="//www.youtube.com/channel/UC8ZqJKbN0diXtBKCRakcxAw/" target="_blank"><i class="icon-youtube"></i></a>
+                <a class="social-behance" href="//behance.net/poweredbypen" target="_blank"><i class="icon-behance"></i></a>
+            </nav>
+        </div>
+    </aside>
+
+    <main class="wrap" role="main">
+        <section class="home-wrap home">
+            <div class="home-content">
+                <div class="slide-controls">
+                    <a class="prev-slide"><i class="icon-navigate_before"></i></a>
+                    <a class="next-slide"><i class="icon-navigate_next"></i></a>
+                </div>
+                <div class="slides"></div>
             </div>
-            <div class="slides"></div>
-        </div>
-    </section>
-    <section class="portfolio portfolio-wrap">
-        <div class="portfolio-contents"></div>
-        <div class="portfolio-loading">
-            <div class="lds-default"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
-        </div>
-    </section>
-    <section class="about about-wrap">
-        <div class="ash"></div>
-        <div class="grntx"></div>
-        <div class="about-content"></div>
-    </section>
-    <section class="contact contact-wrap">
-        <div class="contact-content"></div>
-        <div class="pbyplogo"></div>
-    </section>
-</main>
-<?php get_footer(); ?>
+        </section>
+        <section class="portfolio portfolio-wrap">
+            <div class="portfolio-contents"></div>
+            <div class="portfolio-loading">
+                <div class="lds-default"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div></div>
+            </div>
+        </section>
+        <section class="about about-wrap">
+            <div class="ash"></div>
+            <div class="grntx"></div>
+            <div class="about-content"></div>
+        </section>
+        <section class="contact contact-wrap">
+            <div class="contact-content"></div>
+            <div class="pbyplogo"></div>
+        </section>
+    </main>
+
+    <div id="modal">
+		<article class="project-container" data-name="" >
+		    <header class="project-details">
+				<a class="close-modal" title="Back to project"><i class="icon-close"></i></a>
+		        <div class="project-title"></div>
+		        <div class="project-desc"></div>
+		    </header>
+		    <section class="project-content"></section>
+		</article>
+	</div>
+	
+	<div class="loading">
+		<div class="lds-default"><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><div></div><img src="<?php bloginfo('template_url'); ?>/img/pbyp-logo-small.png"></div>
+	</div>
+
+	<?php wp_footer(); ?>
+</body>
+</html>
