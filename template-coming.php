95897ac11e3fc5999a5a83ee844d1617519b8b2e
Grant Imbo <grant.imbo@gmail.com>
2019-05-27 14:38:02 +0800

    dynamic header

--- template-coming.php
+++ template-coming.php
@@ -1,19 +1,4 @@
-<?php /* Template Name: Coming Soon */ ?>
-
-<!doctype html>
-<html <?php language_attributes(); ?> >
-<head>
-	<meta charset="<?php bloginfo('charset'); ?>">
-	<title>Coming Soon &mdash; <?php bloginfo('description'); ?></title>
-
-	<link href="<?php echo get_template_directory_uri(); ?>/img/icons/favicon.ico" rel="shortcut icon">
-	<link href="<?php echo get_template_directory_uri(); ?>/img/icons/touch.png" rel="apple-touch-icon-precomposed">
-
-	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
-	<meta name="viewport" content="width=device-width,initial-scale=1.0">
-	
-	<?php wp_head(); ?>
-</head>
+<?php get_header(); /* Template Name: Coming Soon */ ?>
 <body style="display: flex; align-items: center; height: 100vh;">
 
 	<main style="text-align: center; margin: 0 auto; width: 340px;">
