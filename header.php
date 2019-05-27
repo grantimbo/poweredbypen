3b9c64c2d6a50d934c1688c3a7473bd3953db582
Grant Imbo <grant.imbo@gmail.com>
2019-05-27 14:37:44 +0800

    add google tag

--- header.php
+++ header.php
@@ -9,6 +9,17 @@
 
 		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
 		<meta name="viewport" content="width=device-width,initial-scale=1.0">
+
+		<!-- Global site tag (gtag.js) - Google Analytics -->
+		<script async src="https://www.googletagmanager.com/gtag/js?id=UA-140911506-1"></script>
+		<script>
+		window.dataLayer = window.dataLayer || [];
+		function gtag(){dataLayer.push(arguments);}
+		gtag('js', new Date());
+
+		gtag('config', 'UA-140911506-1');
+		</script>
+
 		
         <?php wp_head(); ?>
 	</head>
