4c6430026474d23175065e792c7c5202723bdf65
Grant Imbo <grant.imbo@gmail.com>
2019-05-27 13:53:21 +0800

    added coming soon page

---
+++ template-coming.php
@@ -0,0 +1,12 @@
+<?php wp_head(); /* Template Name: Coming Soon */ ?>
+<body style="display: flex; align-items: center;">
+
+	<main style="text-align: center; margin: 0 auto; width: 340px;">
+        <img style="display: inline-block; width: 60px;" src="<?php bloginfo('template_url'); ?>/img/pbyp-logo-small.png" alt="Powered by Pen">
+		<h3 style="color: #545454;">Coming Soon</h3>
+	</main>
+
+	<?php wp_footer(); ?>
+</body>
+</html>
+
